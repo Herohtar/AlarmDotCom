37a93c13e1d780201529f6b034b719b1d1a3fb9d
Herohtar <belac1186@gmail.com>
2019-09-23 23:14:51 -0500

    Update available system items class

--- JsonObjects/AvailableSystemItems.cs
+++ JsonObjects/AvailableSystemItems.cs
@@ -1,76 +1,74 @@
-using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace AlarmDotCom.JsonObjects.AvailableSystemItems
 {
-    public partial class AvailableSystemItems
+    public class AvailableSystemItems
     {
-        [JsonProperty("data")]
+        [JsonPropertyName("data")]
         public List<Datum> Data { get; set; }
 
-        [JsonProperty("included")]
+        [JsonPropertyName("included")]
         public List<object> Included { get; set; }
 
-        [JsonProperty("meta")]
+        [JsonPropertyName("meta")]
         public AvailableSystemItemsMeta Meta { get; set; }
+
+        public static AvailableSystemItems FromJson(string json) => JsonSerializer.Deserialize<AvailableSystemItems>(json);
     }
 
-    public partial class Datum
+    public class AvailableSystemItemsMeta
     {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("type")]
-        public string Type { get; set; }
-
-        [JsonProperty("attributes")]
-        public Attributes Attributes { get; set; }
-
-        [JsonProperty("relationships")]
-        public Relationships Relationships { get; set; }
-    }
-
-    public partial class Attributes
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("icon")]
-        public string Icon { get; set; }
-
-        [JsonProperty("isSelected")]
-        public bool IsSelected { get; set; }
-    }
-
-    public partial class Relationships
-    {
-        [JsonProperty("subItems")]
-        public SubItems SubItems { get; set; }
-    }
-
-    public partial class SubItems
-    {
-        [JsonProperty("data")]
-        public List<object> Data { get; set; }
-
-        [JsonProperty("meta")]
-        public SubItemsMeta Meta { get; set; }
-    }
-
-    public partial class SubItemsMeta
-    {
-        [JsonProperty("count")]
-        public string Count { get; set; }
-    }
-
-    public partial class AvailableSystemItemsMeta
-    {
-        [JsonProperty("transformer_version")]
+        [JsonPropertyName("transformer_version")]
         public string TransformerVersion { get; set; }
     }
 
-    public partial class AvailableSystemItems
+    public class Datum
     {
-        public static AvailableSystemItems FromJson(string json) => JsonConvert.DeserializeObject<AvailableSystemItems>(json, Converter.Settings);
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("attributes")]
+        public Attributes Attributes { get; set; }
+
+        [JsonPropertyName("relationships")]
+        public Relationships Relationships { get; set; }
+    }
+
+    public class Attributes
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; }
+
+        [JsonPropertyName("isSelected")]
+        public bool IsSelected { get; set; }
+    }
+
+    public class Relationships
+    {
+        [JsonPropertyName("subItems")]
+        public SubItems SubItems { get; set; }
+    }
+
+    public class SubItems
+    {
+        [JsonPropertyName("data")]
+        public List<object> Data { get; set; }
+
+        [JsonPropertyName("meta")]
+        public SubItemsMeta Meta { get; set; }
+    }
+
+    public class SubItemsMeta
+    {
+        [JsonPropertyName("count")]
+        public string Count { get; set; }
     }
 }
