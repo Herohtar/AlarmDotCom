fbcb7888e28f9bcd19164a3de4b165919410670c
Herohtar <belac1186@gmail.com>
2019-10-27 13:31:17 -0500

    Use KeepAliveResultConverter

--- JsonObjects/KeepAliveResponse.cs
+++ JsonObjects/KeepAliveResponse.cs
@@ -8,6 +8,12 @@ namespace AlarmDotCom.JsonObjects
         [JsonPropertyName("status")]
         public KeepAliveResult Status { get; set; }
 
-        public static KeepAliveResponse FromJson(string json) => JsonSerializer.Deserialize<KeepAliveResponse>(json);
+        public static KeepAliveResponse FromJson(string json)
+        {
+            var options = new JsonSerializerOptions();
+            options.Converters.Add(new KeepAliveResultConverter());
+
+            return JsonSerializer.Deserialize<KeepAliveResponse>(json, options);
+        }
     }
 }
